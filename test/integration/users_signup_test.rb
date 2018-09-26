63198f38b7ed36b0362845540b2feb149ed303da
Yash Kirloskar <yashvkirloskar@berkley.edu>
2018-09-26 19:06:31 +0000

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+    assert_select 'form[action="/signup"]'
+    assert_difference 'User.count' do
+      post users_path, params: { user: { name: "Example",
+                                         email: "example@example.com",
+                                         password: "foobar",
+                                         password_confirmation: "foobar" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    
+    assert_not flash.empty?
+  end
+end
