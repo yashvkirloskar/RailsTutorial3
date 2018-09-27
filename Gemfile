82575df9491d1396e66b5fdf3ab909fac78ece3b
Yash Kirloskar <yashvkirloskar@berkley.edu>
2018-09-27 20:42:05 +0000

    Implement basic login

--- Gemfile
+++ Gemfile
@@ -8,7 +8,8 @@ end
 source 'https://rubygems.org'
 
 gem 'bootstrap-sass', '~> 3.3.7'
-
+gem 'jquery-rails', '~> 4.3', '>= 4.3.3'
+gem 'rails-ujs', '~> 0.1.0'
 gem 'bcrypt',         '3.1.12'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,4 +9,13 @@ class ActiveSupport::TestCase
   fixtures :all
   include ApplicationHelper
   # Add more helper methods to be used by all tests here...
+  
+  class ActiveSupport::TestCase
+    fixtures :all
+  
+    # Returns true if a test user is logged in.
+    def is_logged_in?
+      !session[:user_id].nil?
+    end
+  end
 end
